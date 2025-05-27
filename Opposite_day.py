a9fced602d09bb7548cfe779dd291fcf1cd821f6
Puke684 <ilikeapples684@gmail.com>
2025-05-26 21:57:59 -0500

    Update Opposite_day.py

--- Opposite_day.py
+++ Opposite_day.py
@@ -1,7 +1,19 @@
+import time
+
+print('list things you like, dislike, and love.')
+time.sleep(1)
+
 a = input('you like to...')
+
 b = input('you dislike...')
+
 c = input('you would love to...')
 
+
 print(f'you hate to {a}')
+time.sleep(1)
+
 print(f'you love {b}')
+time.sleep(1)
+
 print(f'you would hate to {c}')
